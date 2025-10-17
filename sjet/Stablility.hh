9a262fff5b69eeee2d9ff4f3a750a0abe60de2cc
smagedov <smagedov@ttu.edu>
2025-10-17 09:39:22 -0500

    fixed Stabiltiy func

--- sjet/Stablility.hh
+++ sjet/Stablility.hh
@@ -5,11 +5,9 @@
 
 namespace sjet {
 	template <typename stability>
-	double stability(double dist1, double dist2) {
-		double s = std::log(dist1/dist1)
-	}
-
-	return s;
+	double stability(stability dist1, stability dist2) {
+		return std::log(dist1/dist1)
+	};
 }
 
 #endif //SJET_STABILITYFUNC_HH_
